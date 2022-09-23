95fe9ba11e68cbe8512f91fb703848071fc0e42f
jno3 <joaoribeiro3891@gmail.com>
2022-09-22 21:47:18 -0300

    two arrows

--- sketch.js
+++ sketch.js
@@ -10,17 +10,25 @@ function setup() {
     x: (canvasSize.x / 2) - (8 / 2),
     y: canvasSize.y - 30 * 5.8,
   };
+  arrow2Pos = {
+    x: (canvasSize.x / 2) - (8 / 2),
+    y: canvasSize.y - 30 * 5.8,
+  };
   triggerRight = true;
-  clicked = false;
+  trigger2Right = true;
 }
 
+
+var clicked = false;
+var clicked2 = false;
 function draw() {
   var bgcolor = 'rgba(0,200,0, 1)'
   background(bgcolor);
   // acceptableGoal = goal();
   goal();
   ball();
-  arrow();
+  arrow(clicked);
+  arrow2(clicked, clicked2)
 }
 
 function goal() {
@@ -42,7 +50,7 @@ function ball() {
 }
 
 
-function arrow() {
+function arrow(clicked) {
   var arrowSize = { x: 8, y: 30 };
   rect(
     arrowPos.x,
@@ -50,42 +58,48 @@ function arrow() {
     arrowSize.x,
     arrowSize.y
   )
-  if (arrowPos.x + arrowSize.x > (canvasSize.x/2)+((goalSize.x*1.5)/2)) {
+  if (arrowPos.x + arrowSize.x > (canvasSize.x / 2) + ((goalSize.x * 1.5) / 2)) {
     triggerRight = false;
   }
-  if (arrowPos.x < (canvasSize.x/2)-((goalSize.x*1.5)/2)) {
+  if (arrowPos.x < (canvasSize.x / 2) - ((goalSize.x * 1.5) / 2)) {
     triggerRight = true;
   }
 
-  if (triggerRight) {
+  if (triggerRight && !clicked) {
     arrowPos.x = arrowPos.x + 13;
-  } else if (!triggerRight) {
+  } else if (!triggerRight && !clicked) {
     arrowPos.x = arrowPos.x - 13;
   }
+  return arrowPos.x;
 }
 
-function arrow2() {
+function arrow2(clicked, clicked2) {
   var arrowSize = { x: 8, y: 30 };
   rect(
-    arrowPos.x,
-    arrowPos.y,
+    arrow2Pos.x,
+    arrow2Pos.y,
     arrowSize.x,
     arrowSize.y
   )
-  if (arrowPos.x + arrowSize.x > (canvasSize.x/2)+((goalSize.x*1.5)/2)) {
-    triggerRight = false;
+  if (arrow2Pos.x + arrowSize.x > (canvasSize.x / 2) + ((goalSize.x * 1.5) / 2)) {
+    trigger2Right = false;
   }
-  if (arrowPos.x < (canvasSize.x/2)-((goalSize.x*1.5)/2)) {
-    triggerRight = true;
+  if (arrow2Pos.x < (canvasSize.x / 2) - ((goalSize.x * 1.5) / 2)) {
+    trigger2Right = true;
   }
 
-  if (triggerRight) {
-    arrowPos.x = arrowPos.x + 13;
-  } else if (!triggerRight) {
-    arrowPos.x = arrowPos.x - 13;
+  if (trigger2Right && clicked && !clicked2) {
+    arrow2Pos.x = arrow2Pos.x + 13;
+  } else if (!trigger2Right && clicked && !clicked2) {
+    arrow2Pos.x = arrow2Pos.x - 13;
   }
+  return arrow2Pos.x;
 }
 
-function mouseClicked(){
-  mouseClickedArrow1 = true;
+function mouseClicked() {
+  if (!clicked) {
+    clicked = true;
+  }else{
+    clicked2 = true;
+  }
 }
